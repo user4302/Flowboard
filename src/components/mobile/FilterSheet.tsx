005e4b5ad90f2ae81ac54a6129d04a7b69668ec3
user4302 <user4302@users.noreply.gitlab.com>
2026-06-10 11:47:48 +0530

    style(mobile-nav): allow filter sheet to grow dynamically and simplify layout

--- src/components/mobile/FilterSheet.tsx
+++ src/components/mobile/FilterSheet.tsx
@@ -11,12 +11,13 @@ export function FilterSheet({ boardId }: { boardId: string }) {
     <Modal
       open={showFilterSheet}
       onClose={() => setShowFilterSheet(false)}
+      className="!bg-slate-900/90 !max-h-[90vh] !h-auto !min-h-[200px] !rounded-t-2xl !rounded-b-none !fixed !bottom-0 !top-auto !w-full !max-w-full transition-all duration-300 ease-in-out"
     >
-      <ModalHeader>
-        <ModalTitle>Filters</ModalTitle>
+      <ModalHeader className="border-slate-800">
+        <ModalTitle className="text-white">Filters</ModalTitle>
       </ModalHeader>
-      <ModalBody className="p-4">
-        <SearchAndFilter boardId={boardId} inline={true} />
+      <ModalBody className="p-4 h-full">
+        <SearchAndFilter boardId={boardId} inline={true} className="w-full !relative !static" />
       </ModalBody>
     </Modal>
   );
