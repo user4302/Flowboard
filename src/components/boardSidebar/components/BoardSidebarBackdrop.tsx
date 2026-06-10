0b2f29565eb63f77342219bcde0a8e7436ecd904
user4302 <user4302@users.noreply.gitlab.com>
2026-06-10 10:47:45 +0530

    fix(layout): adjust sidebar backdrop breakpoint

--- src/components/boardSidebar/components/BoardSidebarBackdrop.tsx
+++ src/components/boardSidebar/components/BoardSidebarBackdrop.tsx
@@ -17,7 +17,7 @@ export function BoardSidebarBackdrop({
 
   return (
     <div
-      className="fixed inset-0 z-40 bg-black/50 lg:hidden"
+      className="fixed inset-0 z-40 bg-black/50 md:hidden"
       onClick={onClose}
     />
   );
